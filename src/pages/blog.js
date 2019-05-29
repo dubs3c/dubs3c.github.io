fe08ad7c760495cd4ecbe654476ef37a0dcbe538
mjdubell <mjdubell@users.noreply.github.com>
2019-05-29 22:39:58 +0200

    test

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -116,13 +116,6 @@ export const pageQuery = graphql`
             title
             tags
             categories
-            thumbnail {
-              childImageSharp {
-                fixed(width: 150, height: 150) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
             date
             template
           }
