33947d0e9392b8eb61470cf040ba3071767e4b90
mjdubell <mjdubell@users.noreply.github.com>
2019-05-29 23:00:11 +0200

    lint fix

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
 import Layout from '../layout'
 import UserInfo from '../components/UserInfo'
 import PostTags from '../components/PostTags'
